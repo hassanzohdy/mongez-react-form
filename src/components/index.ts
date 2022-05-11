bfb0904ff7e139b738d17db1b5fe1b5407bb3e2d
hasan <hassanzohdy@gmail.com>
2022-05-11 06:53:50 +0200

    "Added HiddenInput Component"

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Form } from "./Form";
 export { default as FormInput } from "./FormInput";
+export { default as HiddenInput } from "./HiddenInput";
 export { default as ResetFormButton } from "./ResetFormButton";
 export { default as BasicFormInput } from "./FormInput/BasicFormInput";
