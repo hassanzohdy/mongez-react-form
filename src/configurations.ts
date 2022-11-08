b1aba9f0a945078f498589cc00505dcabbdfdaeb
hasan <hassanzohdy@gmail.com>
2022-11-08 09:49:20 +0200

    "Updated packages"

--- src/configurations.ts
+++ src/configurations.ts
@@ -1,16 +1,16 @@
-import { Obj } from "@mongez/reinforcements";
+import { get, merge } from "@mongez/reinforcements";
 import { FormConfigurations } from "./types";
 
 export let formConfigurations: FormConfigurations = {};
 
 export function setFormConfigurations(newConfigurations: FormConfigurations) {
-  formConfigurations = Obj.merge(formConfigurations, newConfigurations);
+  formConfigurations = merge(formConfigurations, newConfigurations);
 }
 
 export function getFormConfig(key: string, defaultValue?: any) {
   if (arguments.length === 0) return formConfigurations;
 
-  return Obj.get(formConfigurations, key, defaultValue);
+  return get(formConfigurations, key, defaultValue);
 }
 
 export function getFormConfigurations(): FormConfigurations {
