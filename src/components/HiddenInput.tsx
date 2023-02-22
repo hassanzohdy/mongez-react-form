b3ca4bfaddceeb21de73fcae9d7f2d80924159dc
hasan <hassanzohdy@gmail.com>
2023-02-22 09:51:09 +0200

    "Hidden input now uses useFormInput hook"

--- src/components/HiddenInput.tsx
+++ src/components/HiddenInput.tsx
@@ -4,19 +4,27 @@ import useFormInput from "../hooks/useFormInput";
 import { FormInputProps, HiddenInputProps } from "../types";
 
 export default function HiddenInput(props: HiddenInputProps & FormInputProps) {
-  const { name, value } = useFormInput(props);
+  const { name, value, id } = useFormInput(props);
 
   if (!name) return null;
 
   if (!Array.isArray(value)) {
-    return <input type="hidden" name={toInputName(name)} value={value || ""} />;
+    return (
+      <input
+        type="hidden"
+        id={id}
+        name={toInputName(name)}
+        value={value || ""}
+      />
+    );
   }
 
   return (
     <>
-      {value.map((singleValue: any) => (
+      {value.map((singleValue: any, index: number) => (
         <input
           type="hidden"
+          id={id ? `${id}-${index}` : undefined}
           key={String(singleValue)}
           name={toInputName(name)}
           value={String(singleValue)}
